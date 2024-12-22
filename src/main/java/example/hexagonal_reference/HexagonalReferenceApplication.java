f3e9ffdcd02d0d5a06b2fb44c4a82832f9b9c78c
Marek Wójcik <m@rekwojcik.com>
2024-12-22 18:58:35 +0100

    Initialized spring boot application

---
+++ src/main/java/example/hexagonal_reference/HexagonalReferenceApplication.java
@@ -0,0 +1,13 @@
+package example.hexagonal_reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalReferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/hexagonal_reference/HexagonalReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package example.hexagonal_reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
